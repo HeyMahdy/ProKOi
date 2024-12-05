2c6f82a9d28fa3e34839fca3775517aa2e6236f1
Mahdy Muzzammil <156871399+HeyMahdy@users.noreply.github.com>
2024-12-05 20:25:30 +0600

    project started

---
+++ src/test/java/com/example/ProKOi/ProKOiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProKOi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProKOiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
